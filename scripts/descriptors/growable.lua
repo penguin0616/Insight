4cc61ea98c094c1e283a720ba424bd95b2c8b8dd
penguin0616 <23770056+penguin0616@users.noreply.github.com>
2022-09-07 22:31:22 -0500

    Added advanced hover for growables.

--- scripts/descriptors/growable.lua
+++ scripts/descriptors/growable.lua
@@ -20,44 +20,51 @@ directory. If not, please refer to
 
 -- growable.lua
 local function Describe(self, context)
-	local inst = self.inst
-	local description = nil
+	local description, alt_description
 	local verbosity = context.config["growth_verbosity"]
+
+	if verbosity == 0 then
+		return
+	end
 
 	-- self.pausedremaining = (n) or nil
 	-- self.stage = (n)
 	-- self.stages = (t) -> {name = (s), ...}
 	-- self.targettime
 
-	if verbosity > 0 and self:GetCurrentStageData() then -- 0 == none
-		if verbosity == 1 then -- 1 == minimal
-			description = ""
-		elseif verbosity == 2 then -- 2 == all
-			description = string.format(context.lstr.growable.stage, self:GetCurrentStageData().name or "MISSING_NAME", self.stage, #self.stages)
-		end
+	local stage_data = self:GetCurrentStageData()
+	if not stage_data then
+		return
+	end
 
-		if self.pausedremaining then
+	description = ""
+	alt_description = string.format(context.lstr.growable.stage, stage_data.name or "MISSING_NAME", self.stage, #self.stages)
+
+	if self.pausedremaining then
+		description = description .. context.lstr.growable.paused
+		alt_description = alt_description .. context.lstr.growable.paused
+		
+	elseif self.targettime and context.config["time_style"] ~= "none" then -- we need a target time so.
+		local remaining_time = self.targettime - GetTime()
+
+		-- times can be from 1 frame short to 935 seconds short. nice.
+		if self.task and remaining_time > 0 then
+			remaining_time = context.time:SimpleProcess(remaining_time)
+			remaining_time = string.format(context.lstr.growable.next_stage, remaining_time)
+
+			description = description .. remaining_time
+			alt_description = alt_description .. remaining_time
+		else
+			--description = string.format("%sGrowth paused.", description)
 			description = description .. context.lstr.growable.paused
-			
-		elseif self.targettime and context.config["time_style"] ~= "none" then -- we need a target time so.
-			local remaining_time = self.targettime - GetTime()
-
-			-- times can be from 1 frame short to 935 seconds short. nice.
-			if self.task and remaining_time > 0 then
-				remaining_time = context.time:SimpleProcess(remaining_time)
-				--description = string.format("%sNext stage in %s.", description, remaining_time)
-				description = description .. string.format(context.lstr.growable.next_stage, remaining_time)
-			else
-				--description = string.format("%sGrowth paused.", description)
-				description = description .. context.lstr.growable.paused
-			end
+			alt_description = alt_description .. context.lstr.growable.paused
 		end
 	end
 
-
 	return {
 		priority = 5,
-		description = description
+		description = description,
+		alt_description = alt_description
 	}
 end
 
