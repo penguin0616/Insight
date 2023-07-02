e7c8f86c80afe468f9c3d07b63a31ea292f622ed
penguin0616 <23770056+penguin0616@users.noreply.github.com>
2023-07-01 23:50:18 -0500

    Cleanup, seems to work.
    Just needs to be smoothed-out

--- scripts/prefab_descriptors/voidcloth_scythe.lua
+++ scripts/prefab_descriptors/voidcloth_scythe.lua
@@ -30,6 +30,16 @@ local function OnClientInit()
 
 end
 
+local function GetAngleFromAToB(A, B)
+	-- Math from EntitiyScript:GetAngleToPoint, Idea from searching angle in locomotor
+	-- The 180 + seems unncessary.
+	
+	--local x1, y1, z1 = doer_pos:Get()
+	--local x, y, z = target_pos:Get()
+	-- local angle = math.atan2(z1 - z, x - x1) * RADIANS
+	return math.atan2(A.z - B.z, B.x - A.x) * RADIANS
+end
+
 -- This function from Klei
 local function IsEntityInFront(inst, entity, doer_rotation, doer_pos)
     local facing = Vector3(math.cos(-doer_rotation / RADIANS), 0 , math.sin(-doer_rotation / RADIANS))
@@ -37,12 +47,11 @@ local function IsEntityInFront(inst, entity, doer_rotation, doer_pos)
     return IsWithinAngle(doer_pos, facing, TUNING.VOIDCLOTH_SCYTHE_HARVEST_ANGLE_WIDTH, entity:GetPosition())
 end
 
-est = SpawnPrefab("nightstick")
+--est = SpawnPrefab("nightstick")
 
 
 
 local function OnScytheTargetSelected(inst, target, doer)
-	print('SELECTED', target, inst, doer)
 	scything[doer] = {}
 
 	--target.AnimState:SetAddColour(0.1, 0.1, 0.3, 0.3)
@@ -54,7 +63,6 @@ local function OnScytheTargetSelected(inst, target, doer)
 	local doer_pos = doer:GetPosition()
 	local target_pos = target:GetPosition()
 	local estimated_pos
-	local rotation
 
 	local v1 = doer_pos
 	local v2 = target_pos
@@ -66,98 +74,37 @@ local function OnScytheTargetSelected(inst, target, doer)
 	local magnitude = math.sqrt(dx*dx + dy*dy + dz*dz)
 
 	if magnitude > reap_dist then
-		print(string.rep("=", 90))
-		--print("diffs", dx, dy, dz)
-
 		local a, b, c = (dx / magnitude), (dy / magnitude), (dz / magnitude)
-		--print("percent", a, b, c)
-
-
 
 		estimated_pos = Vector3(
 			Lerp(doer_pos.x, target_pos.x + reap_dist * a, 1),
 			Lerp(doer_pos.y, target_pos.y + reap_dist * b, 1),
 			Lerp(doer_pos.z, target_pos.z + reap_dist * c, 1)
 		)
-
-		est.Transform:SetPosition(estimated_pos:Get())
 	else
 		-- yay!!
 		estimated_pos = doer_pos
 	end
 
 
-
-	-- Math from EntitiyScript:GetAngleToPoint, Idea from searching angle in locomotor
-	-- The 180 + seems unncessary.
-	local x1, y1, z1 = doer_pos:Get() -- If you do it from estimated_pos, it'll be facing from the 'nightstick' example
+ 	-- If you do it from estimated_pos, it'll be facing from the 'nightstick' example
 	-- Do it from the doer_pos so it'll be as if the target walked there
-	local x, y, z = target_pos:Get()
-	local angle = math.atan2(z1 - z, x - x1) * RADIANS
+	local estimated_rotation = GetAngleFromAToB(doer_pos, target_pos)
 
-	print("current", ThePlayer.Transform:GetRotation())
-	rotation = angle
-
-
-	-- Math from Klei
+	-- Logic from Klei
 	local x, y, z = estimated_pos:Get()
 
 	local ents = TheSim:FindEntities(x, y, z, TUNING.VOIDCLOTH_SCYTHE_HARVEST_RADIUS, HARVEST_MUSTTAGS, HARVEST_CANTTAGS, HARVEST_ONEOFTAGS)
 	for _, ent in pairs(ents) do
 		if ent:IsValid() then
-			if IsEntityInFront(inst, ent, rotation, estimated_pos) then
+			if IsEntityInFront(inst, ent, estimated_rotation, estimated_pos) then
 				scything[doer][ent] = true
 				--ent.AnimState:SetLightOverride(0)
 				--ent.AnimState:SetMultColour(1, 0.3, 0.3, 1)
-				ent.AnimState:SetAddColour(0.3, 0.1, 0.1, 0.2)
+				ent.AnimState:SetAddColour(0.3, 0.1, 0.1, 0.3)
 			end
 		end
 	end
-
-		--[[
-		-- How much does each contribute to the magnitude?
-		-- Absolute value fixes the issues where some sides would actually get further away
-		local a, b, c = math.abs(dx / magnitude), dy / magnitude, math.abs(dz / magnitude)
-
-		-- Turn that into distance to add
-		local dxp, dyp, dzp = dx * a, dy * b, dz * c
-
-		print("percent", a, b, c)
-		print("addition", Vector3(dxp, 0, dzp))
-
-		print(string.rep("-", 90))
-		estimated_pos = doer_pos - Vector3(dxp, dyp, dzp)
-		est.Transform:SetPosition(estimated_pos:Get())
-		
-		print("doer", doer_pos)
-		print("estimated", estimated_pos)
-		print("target", target_pos)
-
-		print(magnitude, "VS", math.sqrt(target:GetDistanceSqToPoint(estimated_pos)))
-		print(string.rep("=", 90))
-		--]]
-
-	
-
-
-	--[[
-	-- Math from Klei
-	local doer_pos = doer:GetPosition()
-	local x, y, z = doer_pos:Get()
-	local doer_rotation = doer.Transform:GetRotation()
-
-	local ents = TheSim:FindEntities(x, y, z, TUNING.VOIDCLOTH_SCYTHE_HARVEST_RADIUS, HARVEST_MUSTTAGS, HARVEST_CANTTAGS, HARVEST_ONEOFTAGS)
-	for _, ent in pairs(ents) do
-		if ent:IsValid() then
-			if IsEntityInFront(inst, ent, doer_rotation, doer_pos) then
-				scything[doer][ent] = true
-				--ent.AnimState:SetLightOverride(0)
-				--ent.AnimState:SetMultColour(1, 0.3, 0.3, 1)
-				ent.AnimState:SetAddColour(0.3, 0.1, 0.1, 0.2)
-			end
-		end
-	end
-	--]]
 end
 
 local function OnScytheTargetUnselected(target, doer)
