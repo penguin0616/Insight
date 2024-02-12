32aa3ae2d26d5153d13932568b25c88522223071
penguin0616 <23770056+penguin0616@users.noreply.github.com>
2024-02-12 11:32:01 -0600

    Bump steam version

--- modinfo.lua
+++ modinfo.lua
@@ -46,7 +46,7 @@ end
 
 name = "Insight"
 -- Major.Minor.Patch
-version = "4.5.0" -- dst is 4.4.5, ds is 4.4.5
+version = "4.5.1" -- dst is 4.4.5, ds is 4.4.5
 author = "penguin0616"
 forumthread = ""
 icon_atlas = "modicon.xml"
