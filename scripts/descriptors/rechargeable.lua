4e243411262a0b33b45ff753e97734d948dfb611
penguin0616 <23770056+penguin0616@users.noreply.github.com>
2025-01-10 18:57:18 -0600

    Fix rechargeable information not showing

--- scripts/descriptors/rechargeable.lua
+++ scripts/descriptors/rechargeable.lua
@@ -19,8 +19,9 @@ directory. If not, please refer to
 ]]
 
 -- rechargeable.lua
-local ST = Color.fromHex("#0069A7")
-local EN = Color.fromHex("#ffffff") --Color.fromHex("#D1FFFF")
+--local start_color = Color.fromHex("#0069A7") -- Interesting choice of start color, I guess.
+local start_color = Color.fromHex("#504375") -- Recharge color is #504375, so we'll try that.
+local end_color = Color.fromHex("#ffffff") --Color.fromHex("#D1FFFF")
 
 local function Describe(self, context)
 	local description, alt_description = nil, nil
@@ -28,14 +29,24 @@ local function Describe(self, context)
 	-- myth words theme has a myth_rechargeable component that screws with this.
 	-- why insight picks up a component with that name as rechargeable, i don't know.
 	-- they probably did something dumb.
-	if not self.GetTimeToCharge or not self.current or not self.total then
+	if not self.GetTimeToCharge or type(self.current) ~= "number" or type(self.total) ~= "number" then
 		return
 	end
 
 	local percent = self.current / self.total
 	percent = not isbadnumber(percent) and percent or 0
 
-	local remaining = self:GetTimeToCharge()
+	local remaining_charge_time = self:GetTimeToCharge()
+
+	if remaining_charge_time >= 0 then
+		description = string.format(context.lstr.rechargeable.charged_in, 
+			ApplyColor(context.time:SimpleProcess(remaining_charge_time), start_color:Lerp(end_color, percent))
+		)
+		alt_description = string.format(context.lstr.rechargeable.charge, Round(self.current, 0), Round(self.total, 0)) .. ", " .. description
+	end
+
+	--[[
+	-- What was going on here?
 	if remaining / remaining == remaining then -- widgets/itemtile.lua:411
 		description = string.format(context.lstr.rechargeable.charged_in, 
 			ApplyColor(context.time:SimpleProcess(remaining), ST:Lerp(EN, percent))
@@ -46,6 +57,7 @@ local function Describe(self, context)
 	if not (remaining > 0) then
 		description = nil
 	end
+	--]]
 
 	return {
 		priority = 10,
