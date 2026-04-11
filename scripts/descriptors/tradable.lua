711d5f6c6236da34016dcb6003d91c6bc3f2534c
penguin0616 <23770056+penguin0616@users.noreply.github.com>
2026-04-10 23:02:01 -0500

    Fix tradable not properly getting numerical values

--- scripts/descriptors/tradable.lua
+++ scripts/descriptors/tradable.lua
@@ -21,9 +21,9 @@ directory. If not, please refer to
 -- tradable.lua
 local function Describe(self, context)
 	local description = nil
-	local gold = type(self.goldvalue) == "number" and self.goldvalue > 0
-	local dubloon = type(self.dubloonvalue) == "number" and self.dubloonvalue > 0
-	local tribute = type(self.rocktribute) == "number" and self.rocktribute > 0
+	local gold = type(self.goldvalue) == "number" and self.goldvalue > 0 and self.goldvalue or nil
+	local dubloon = type(self.dubloonvalue) == "number" and self.dubloonvalue > 0 and self.dubloonvalue or nil
+	local tribute = type(self.rocktribute) == "number" and self.rocktribute > 0 and self.rocktribute or nil
 
 
 	if context.config["item_worth"] then
