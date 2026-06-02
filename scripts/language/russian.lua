a68cb2be3a5062fb71d0393117ebf29b416dbd83
penguin0616 <23770056+penguin0616@users.noreply.github.com>
2026-06-01 20:16:12 -0500

    Add containerinstallableitem for other languages

--- scripts/language/russian.lua
+++ scripts/language/russian.lua
@@ -378,6 +378,21 @@ return {
 		
 	},
 
+	-- containerinstallableitem.lua
+	containerinstallableitem = {
+		slingshot_band = {
+			range = "Increases range by <color=FRUIT>%s</color>",
+			speed = "Increases projectile speed by <color=FRUIT>%.0f%%</color>",
+			slingshot_band_mimic = "%d%% chance not to consume ammo.",
+		},
+		slingshot_handle = {
+			fire_rate = "%.1f/s",
+			fire_rate_ramping = "%.1f/s → %.1f/s",
+			primary_fire_rate = "Primary fire rate: %s",
+			secondary_fire_rate = "Secondary fire rate: %s",
+		},
+	},
+
 	-- cooldown.lua
 	cooldown = "Перезарядка: %s",
 
